035aa51966fb2e451b1ab17c0960f2500f6aa92a
hss01248 <hss012489@gmail.com>
2020-09-03 11:09:00 +0800

    不再删除chuck.db,避免影响真正的chuck库

--- library/src/main/java/com/chuckerteam/chucker/internal/data/room/ChuckerDatabase.kt
+++ library/src/main/java/com/chuckerteam/chucker/internal/data/room/ChuckerDatabase.kt
@@ -12,13 +12,10 @@ internal abstract class ChuckerDatabase : RoomDatabase() {
     abstract fun throwableDao(): RecordedThrowableDao
 
     companion object {
-        private const val OLD_DB_NAME = "chuck.db"
-        private const val DB_NAME = "chucker.db"
+        private const val DB_NAME = "chuckerexception.db"
 
         fun create(context: Context): ChuckerDatabase {
             // We eventually delete the old DB if a previous version of Chuck/Chucker was used.
-            context.getDatabasePath(OLD_DB_NAME).delete()
-
             return Room.databaseBuilder(context, ChuckerDatabase::class.java, DB_NAME)
                 .fallbackToDestructiveMigration()
                 .build()
